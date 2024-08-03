1a40cc3925c580ee55a68f1715a4f1474e41be27
nithinpublic <nithinpublic@gmail.com>
2024-08-03 01:03:21 +0100

    json works!

--- js/parser.js
+++ js/parser.js
@@ -1,14 +1,26 @@
 console.clear();
 
+//fetch stuff: https://www.youtube.com/watch?v=cuEtnrL9-H0
 
 fetch("csv/curated_reads.json")
     .then(response => {
-         if (response.ok) { 
-            console.log("Success") 
-        } else { 
-            console.log("Fetch failed") 
-        } 
-    })
+        if (response.ok) { 
+           console.log("Success") 
+           return response.json()
+           
+           
+       } else { 
+           console.log("Fetch failed") 
+           return response.json()
+       } 
+   })
     .then(data => {
-        console.log(data)
+        write_card(data)
     })
+
+function write_card(data){
+    console.log("writing card", data)
+    index = 1
+    document.getElementById("article-title").innerHTML = data[index].title;
+    document.getElementById("article-description").innerHTML = data[index].description;
+}
