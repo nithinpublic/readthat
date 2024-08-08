989880e6119c4e950681d762797408c8dc6efba2
nithinpublic <nithinpublic@gmail.com>
2024-08-08 04:01:04 +0100

    week logic added

--- js/parser.js
+++ js/parser.js
@@ -4,7 +4,34 @@ document.onload = fetch_and_fill_card();
 
 function fetch_and_fill_card() {
     console.clear();
-    console.log("Fetching")
+
+    switch (new Date().getDay()) {
+        case 0:
+            console.log("Its a Sunday");
+            break;
+        case 1:
+            console.log("Its a Monday");
+            break;
+        case 2:
+            console.log("Its a Tuesday");
+            break;
+        case 3:
+            console.log("Its a Wednesday");
+            break;
+        case 4:
+            console.log("Its a Thursday");
+            break;
+        case 5:
+            console.log("Its a Friday");
+            break;
+        case 6:
+            console.log("Its a Saturday");
+            break;
+
+    }
+
+
+    console.log("Fetching data...")
 
 
     //need to set up the logic for fetching the write json bases on day of the week
@@ -79,6 +106,10 @@ function write_card(data) {
     setup_share_button(data[index].title, data[index].link);
 
 }
+
+
+
+
 var offset = 213 // change this to const and put it inside the day_of_the_year function when removing the dev-tool-clicker
 
 
@@ -86,12 +117,29 @@ var offset = 213 // change this to const and put it inside the day_of_the_year f
 
 
 function day_of_the_year() {
-    today = new Date()
+    today = new Date();
 
-    // this is to adjust to the fact that I'm starting this today (3 August 2024) hehe
-    dayIntoYear = (Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()) - Date.UTC(today.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000
-    console.log(dayIntoYear)
+    // the offset is to adjust to the fact that I'm starting this today (3 August 2024) hehe
+    dayIntoYear = (Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()) - Date.UTC(today.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
+    console.log(dayIntoYear);
+
     return dayIntoYear - offset;
+
+}
+
+function week_of_the_year(fromThisDay = 0) {
+    var now = new Date();
+    var start = new Date(now.getFullYear(), 0, 0);
+    var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+    var oneDay = 1000 * 60 * 60 * 24;
+    var dayOfYear = Math.floor(diff / oneDay) - fromThisDay;
+    console.log('Day of year: ' + dayOfYear);
+
+    //calculate week of the year
+    var weekOfYear = Math.floor(dayOfYear * (52 / 365)) + 1;
+    console.log("Week of the year with DST: " + (weekOfYear));
+
+    return weekOfYear;
 }
 
 function extractDomain(url) {
@@ -99,7 +147,7 @@ function extractDomain(url) {
 }
 
 
-function setup_share_button(titleToShare, linkToShare){
+function setup_share_button(titleToShare, linkToShare) {
     if (navigator.share) {
         const shareButton = document.getElementById('share-button');
         shareButton.addEventListener('click', async () => {
@@ -110,7 +158,7 @@ function setup_share_button(titleToShare, linkToShare){
                     text: 'Shared via Read & Crumb',
                     url: linkToShare
                 });
-    
+
                 console.log('Shared successfully');
             } catch (error) {
                 console.error('Error sharing:', error.message);
